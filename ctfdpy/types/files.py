052b9feb851e40e073d85f69f661f7525d6fb3b4
Jus-Codin <70018166+Jus-Codin@users.noreply.github.com>
2025-03-04 20:19:24 +0800

    feat: accept any type with read bytes

--- ctfdpy/types/files.py
+++ ctfdpy/types/files.py
@@ -1,15 +1,15 @@
-from typing import Mapping
+from typing import Mapping, Protocol, TypedDict, runtime_checkable
 
-from typing_extensions import TypedDict
 
 # We have to do this as file objects cannot be properly type hinted for pydantic
 # See https://github.com/pydantic/pydantic/issues/5443
-with open(__file__, "rb") as f:
-    # Note that this doesn't include files opened in write mode
-    BinaryFileReader = type(f)
+@runtime_checkable
+class SupportsReadBytes(Protocol):
+    def read(self, size: int) -> bytes: ...
+
 
 # From https://github.com/encode/httpx/blob/392dbe45f086d0877bd288c5d68abf860653b680/httpx/_types.py#L96-L106
-FileContent = BinaryFileReader | bytes | str
+FileContent = SupportsReadBytes | bytes | str
 MultipartFileTypes = (
     # file (or bytes)
     FileContent
